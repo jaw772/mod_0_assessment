c1345009d14bdad359143af61b5399da3ad9f8de
Josh Walsh <dmaggioarcangeli@yahoo.com>
2021-07-30 16:55:59 -0600

    Create cottage hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+cottage = {
+  "location" = "United Kingdom",
+  "color" = "Brown",
+  "humidity" = 0.90,
+  "number_of_windows" = 6,
+  "is_humid" = true,
+  "is_warm" = false,
+  "types_of_cars_in_garage" = ["Jaguar", "Land Rover", "Bentley"],
+  "furniture_bedroom" = ["Bed", "Night stand", "Dresser", "Mirror"],
+}
