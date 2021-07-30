acef0eb1ee830a38e5166dce0ad8ec01b83c176d
Josh Walsh <dmaggioarcangeli@yahoo.com>
2021-07-30 16:47:27 -0600

    Create macbook hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+macbook = {
+  "manufacturer" = "Apple",
+  "model_type" = "Pro M1",
+  "production_year" = 2020,
+  "memory_in_gb" = 8,
+  "can_connect_to_iphone" = true,
+  "is_fast" = true,
+  "components" = ["Processor", "GPU", "Hard Drive"],
+  "browsers" = ["Safari", "Chrome"],
+}
